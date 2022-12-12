17598719cb0a80e183516b1aea69cdac88e6bae1
Connor Bell <ronnoclleb@gmail.com>
2022-12-12 12:25:42 -0500

    restructured movement

--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -1,18 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Controls : MonoBehaviour {
     // TODO only these support touch, or only these show touch upoon controller disconnect?
-    static readonly RuntimePlatform[] TouchPlatforms = { 
+    static readonly RuntimePlatform[] TouchPlatforms = {
         RuntimePlatform.Android, RuntimePlatform.Switch, RuntimePlatform.IPhonePlayer,
     };
 
+    public static float HorizontalInput;
+    public Player player;
+
     // Start is called before the first frame update
     void Start() {
+        HorizontalInput = 0;
     }
 
     // Update is called once per frame
     void Update() {
+        // TODO if game being played
+        GetInput();
+        player.UpdatePosition(HorizontalInput);
+    }
+
+    void GetInput() {
+        // TODO keyboard
+        if (
+            Array.Exists(TouchPlatforms, p => p == Application.platform) &&
+            false
+        ) {
+            // TODO if touch platform and no controller connected
+            // TODO touch controls
+        } else {
+            HorizontalInput = Input.GetAxis("Horizontal");
+        }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,10 +11,13 @@ public class Player : MonoBehaviour {
     float angle;
     public float layer;
     static readonly float DEG_PER_SEC = 40f; // deg per sec
-    static readonly float UNITS_PER_SEC = Util.ArcLength(Block.RingRad, DEG_PER_SEC);
+    static readonly float H_UNITS_PER_SEC = Util.ArcLength(Block.RingRad, DEG_PER_SEC);
+    static readonly float V_UNITS_PER_SEC = 2 * H_UNITS_PER_SEC;
+    static readonly float G_UNITS_PER_SEC = 0.5f * V_UNITS_PER_SEC;
 
     PlayerSensor playerSensor;
     HorizontalSensor horizontalSensor;
+
 
     // Start is called before the first frame update
     void Start() {
@@ -30,54 +33,40 @@ public class Player : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        float verticalChange = 0;
-        float layerChange = 0;
+    }
 
+    public void UpdatePosition(float horizontalInput) {
         if (!playerSensor.colliding) {
             // if there is nothing below player
-            // TODO do this bc player movement should completely overwrite gravity... right?
-            verticalChange = -UNITS_PER_SEC * Time.deltaTime;
-            layerChange = -UNITS_PER_SEC * Time.deltaTime;
+            // TODO player movement should completely overwrite gravity... right?
+            transform.Translate(new Vector3(0, -G_UNITS_PER_SEC * Time.deltaTime, 0));
         } else if (layer - Mathf.Floor(layer) > 0) {
             Debug.Log(layer - Mathf.Floor(layer));
             // if there is something below the player but the palyer is not evenly on a layer
-            verticalChange = -Mathf.Min(UNITS_PER_SEC * Time.deltaTime, layer-Mathf.Floor(layer));
-            layerChange = -Mathf.Min(UNITS_PER_SEC * Time.deltaTime, layer-Mathf.Floor(layer));
+            transform.Translate(new Vector3(0, -Mathf.Min(G_UNITS_PER_SEC * Time.deltaTime, layer - Mathf.Floor(layer)),
+                0));
         }
 
-
-        var horizontalInput = Input.GetAxis("Horizontal"); // -1 to 1
         if (horizontalInput != 0) {
-            int angleMod = Util.LogicallyCorrectModulus((int)angle, 20);
-            if (horizontalSensor.isColliding && angleMod == 0) {
-                verticalChange = UNITS_PER_SEC * Mathf.Abs(horizontalInput) * Time.deltaTime;
-                layerChange = UNITS_PER_SEC * Mathf.Abs(horizontalInput) * Time.deltaTime;
+            if (horizontalSensor.isColliding && Math.Abs(Util.LogicallyCorrectModulus((int)angle, 20)) == 0) {
+                transform.Translate(new Vector3(0, V_UNITS_PER_SEC * Mathf.Abs(horizontalInput) * Time.deltaTime, 0));
             } else {
                 transform.RotateAround(Vector3.zero, Vector3.up, -DEG_PER_SEC * horizontalInput * Time.deltaTime);
                 angle += DEG_PER_SEC * horizontalInput * Time.deltaTime;
             }
-
-            UpdateVertical(verticalChange, layerChange);
-
-            // set sensor position to the position of the block with the majority of the player over it
-            Block.GoToPosition(playerSensor.transform, angle - angleMod + (angleMod < 10 ? 0 : 20), layer - 1);
-            Block.GoToPosition(horizontalSensor.transform,
-                angle - angleMod + (angleMod < 10 ? 0 : 20) + (horizontalInput > 0 ? 20 : -20),
-                Mathf.Floor(layer));
-        } else {
-            UpdateVertical(verticalChange, layerChange);
         }
+
+        layer = transform.position.y;
+
+        int angleMod = Util.LogicallyCorrectModulus((int)angle, 20);
+        // set sensor position to the position of the block with the majority of the player over it
+        Block.GoToPosition(playerSensor.transform, angle - angleMod + (angleMod < 10 ? 0 : 20), layer - 1);
+        Block.GoToPosition(horizontalSensor.transform,
+            angle - angleMod + (angleMod < 10 ? 0 : 20) + (horizontalInput > 0 ? 20 : -20),
+            Mathf.Floor(layer));
 
 
         Debug.DrawLine(transform.position, playerSensor.transform.position, Color.red);
         Debug.DrawLine(transform.position, horizontalSensor.transform.position, Color.green);
-    }
-
-    void UpdateVertical(float verticalChange, float layerChange) {
-        if (verticalChange != 0) {
-            transform.Translate(new Vector3(0, verticalChange, 0));
-        }
-
-        layer += layerChange;
     }
 }
