b308d6aceda1ee52589602bb2d91442a00ef6854
Connor Bell <ronnoclleb@gmail.com>
2022-12-12 11:47:52 -0500

    why is ui so annoying

--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Controls : MonoBehaviour {
+    // TODO only these support touch, or only these show touch upoon controller disconnect?
+    static readonly RuntimePlatform[] TouchPlatforms = { 
+        RuntimePlatform.Android, RuntimePlatform.Switch, RuntimePlatform.IPhonePlayer,
+    };
+
     // Start is called before the first frame update
     void Start() {
     }
